728434c824a6ca4de96aca0ea64dc12d0f1ee386
SiposPeter103 <peter.sipos@stud.uni-corvinus.hu>
2021-10-20 13:44:38 +0200

    Portfolio listaba

--- UserMaintenance/ValueAtRisk/Form1.cs
+++ UserMaintenance/ValueAtRisk/Form1.cs
@@ -13,13 +13,27 @@ namespace ValueAtRisk
     public partial class Form1 : Form
     {
         List<Tick> Ticks;
+
         PortfolioEntities context = new PortfolioEntities();
+
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList(); //renamed Tick to Ticks hogy mukodjon, valsz nem pipaltam be, hogy "singularize or pluralize..."
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
         }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
